c075bcc8291bf6033d3f89d3b182ce01c01bccd6
Arnald Quezada <arnaldo.quezada@gmail.com>
2021-08-01 18:01:51 -0400

    Primer formulario listo

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Navbar, Container } from 'react-bootstrap'
+
+const Nav = () => {
+
+    return (
+        <div>
+            <Navbar expand="lg" variant="light" bg="light">
+                <Container>
+                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default Nav
